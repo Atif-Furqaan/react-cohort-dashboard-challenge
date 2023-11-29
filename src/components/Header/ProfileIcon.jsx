2a420c1806323da60e6e80c17d145b32f5ceb695
Atif Furqaan <atifurqaan@gmail.com>
2023-11-29 11:14:33 +0000

    Re-submit the challange

---
+++ src/components/Header/ProfileIcon.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+function ProfileIcon () {
+    
+  const [contact, setContact] = useState({})
+  const userName = "AtifFurqaan"
+  const endpointForContacts = `/contact`
+  const baseUrl = `https://boolean-api-server.fly.dev/${userName}`
+  useEffect(() => {
+      fetch(baseUrl + endpointForContacts)
+          .then(res => res.json())
+          .then(data => setContact(data))
+  } , [])
+  let admin = contact[0]
+    if(!admin) {
+        return <div>
+                <h3>LOADING</h3>
+                </div>
+    }
+    return (
+        <div className="profile-icon">
+            <h3>{admin.firstName.charAt(0) + admin.lastName.charAt(0)}</h3>
+        </div>
+    )
+}
+export default ProfileIcon
